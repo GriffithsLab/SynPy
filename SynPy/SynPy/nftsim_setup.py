df9b9ebebec47fec59afda527cdd9272dc1abbfa
Kevin Kadak <49459455+kevinkadak@users.noreply.github.com>
2023-05-14 16:33:22 -0400

    Update nftsim_setup.py

--- SynPy/SynPy/nftsim_setup.py
+++ SynPy/SynPy/nftsim_setup.py
@@ -1,13 +1,12 @@
 import os, sys
 
-def nftsim_setup():
+def nftsim_setup(gcc_version = '9.4.0'):
     if not os.path.exists(os.path.join(os.getcwd(), 'nftsim/')):
         create_nftsim = input('NFTsim not found in local directory.  Clone and make? [y/n] ')
         if create_nftsim == 'y':
             os.system('git clone git@github.com:BrainDynamicsUSYD/nftsim.git')
             os.chdir('nftsim/')
             try: # Assumes being ran from computer cluster with loadable modules (ie. SciNet Niagara)
-                gcc_version = '9.4.0'
                 os.system(f"module load gcc/{gcc_version} && make")    
             except Exception as e:
                 print(e)
