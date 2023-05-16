f3ee92e38ce72f232e682a5cec0a36998f407ebf
Kevin Kadak <49459455+kevinkadak@users.noreply.github.com>
2023-05-16 00:22:40 -0400

    Changed nftsim repo url to public link

--- SynPy/SynPy/nftsim_setup.py
+++ SynPy/SynPy/nftsim_setup.py
@@ -4,7 +4,7 @@ def nftsim_setup(gcc_version = '9.4.0'):
     if not os.path.exists(os.path.join(os.getcwd(), 'nftsim/')):
         create_nftsim = input('NFTsim not found in local directory.  Clone and make? [y/n] ')
         if create_nftsim == 'y':
-            os.system('git clone git@github.com:BrainDynamicsUSYD/nftsim.git')
+            os.system('git clone https://github.com/BrainDynamicsUSYD/nftsim.git')
             os.chdir('nftsim/')
             try: # Assumes being ran from computer cluster with loadable modules (ie. SciNet Niagara)
                 os.system(f"module load gcc/{gcc_version} && make")    
