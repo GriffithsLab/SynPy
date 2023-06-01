89d9c2c0b21e5797b87f6c91da122ac087ca69e1
Kevin Kadak <kada4470@mylaurier.ca>
2023-06-01 02:34:57 -0400

    Adding pip setup file

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+# Read the contents of requirements.txt file
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='SynPy',
+    version='0.1',
+    description='Python wrapper for interfacing with the NFTsim toolbox created by BrainDynamicsUSYD.',
+    author='Kevin Kadak',
+    author_email='kevin.kadak@mail.utoronto.ca',
+    url='https://github.com/GriffithsLab/SynPy',
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
+
+
+
+
+
